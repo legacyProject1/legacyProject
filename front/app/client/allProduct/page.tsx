d38ff6f7e7245ee10aaaf0d388d37f93e9d857fc
marwaZbidi <zbidimarwa0@gmail.com>
2023-12-31 11:56:03 +0100

    modification

--- front/app/client/allProduct/page.tsx
+++ front/app/client/allProduct/page.tsx
@@ -37,16 +37,51 @@ const AllProduct=()=>{
   
 
   return(
+    <div>
+      <div>
+    <div className='flex top-[0] gap-5 ml-[131px] ' >
+    <div className='w-[15rem] h-[15rem] bg-white flex justify-center items-center rounded-xl mb-40'>
+    <div>
+    <img className='w-20 ml-0' src="https://i.pinimg.com/564x/1a/f3/35/1af335096366834d30421aa73a1cd619.jpg" alt="" />
+    <h1 className='text-gray-700 font-bold ml-5 text-[20px]'>Tent</h1>
+    </div>
+  </div>
+  <div className='w-[15rem] h-[15rem] bg-white flex justify-center items-center rounded-xl'>
+    <div>
+    <img className='w-20 ml-0' src="https://i.pinimg.com/564x/35/9b/3c/359b3cffad098ba579159e8e01cf1793.jpg" alt="" />
+    <h1 className='text-gray-700 font-bold ml-1 text-[20px]'>Lantern</h1>
+    </div>
+  </div>
+  <div className='w-[15rem] h-[15rem] bg-white flex justify-center items-center rounded-xl'>
+    <div>
+    <img className='w-25 ml-0' src="https://i.pinimg.com/564x/e2/83/b8/e283b88f539a9fdf8fc8719f5f6037d5.jpg" alt="" />
+    <h1 className='text-gray-700 font-bold ml-10 text-[20px]'>Sleeping Bags</h1>
+    </div>
+  </div>
+  <div className='w-[15rem] h-[15rem] bg-white flex justify-center items-center rounded-xl'>
+    <div>
+    <img className='w-20 ml-5' src="https://i.pinimg.com/564x/52/a5/9e/52a59edaffef49ccf1314bd1d062d933.jpg" alt="" />
+    <h1 className='text-gray-700 font-bold ml-5 text-[20px]'>Flashlight</h1>
+    </div>
+  </div>
+  <div className='w-[15rem] h-[15rem] bg-white flex justify-center items-center rounded-xl'>
+    <div>
+    <img className='w-20 ml-5' src="https://i.pinimg.com/564x/3c/5d/57/3c5d574e49c59f6bfb3b47c75f7189fc.jpg" alt="" />
+    <h1 className='text-gray-700 font-bold ml-5 text-[20px]'>Camp Chairs</h1>
+    </div>
+  </div>
+  </div>
+  </div>
     <div >
     <div className="container mx-auto px-4">
-        <h2 className="text-3xl font-bold text-black mb-8">Introducing Our Latest Product</h2>
+        <h2 className="text-center text-3xl font-bold text-black mb-8">Introducing Our Latest Product</h2>
+        
+        <div className=" grid grid-cols-3 gap-8 " >
         {Data.map((product)=>(
-        <div className=" grid grid-cols-3 gap-8 " key={product.id}>
-            <div className="rounded-lg shadow-lg p-8">                   
-                    <div >
+            <div className="rounded-lg shadow-lg p-8"key={product.id}>                   
+                    <div className="flex items-center justify-center w-64 h-64">
                     <img className="object-cover w-full h-full" src={product.images} alt="Product"/>
                 </div>
-
                 <h3 className="text-xl font-bold text-gray-900 mt-4 text-center">{product.name}</h3>
                 
                 <div className="flex items-center justify-between mt-4">
@@ -55,11 +90,14 @@ const AllProduct=()=>{
                     onClick={()=>{router.push(`/client/oneProduct/${product.name}`)}}
                     >View Product</button>
                     </div>                    
-                </div>      
+                </div>   
+                ))}   
             </div>
-         ))}
+         
     </div>
-</div>
+   </div>
+   
+  </div>
   )
 }
 
--- front/app/client/oneProduct/[name]/page.tsx
+++ front/app/client/oneProduct/[name]/page.tsx
@@ -3,6 +3,7 @@ import React, {useState, useEffect} from 'react'
 import Navbar from '../../navbar/navbar';
 import Footer from '../../component/footer';
 import axios from 'axios';
+import { useParams } from 'next/navigation';
 
 
 interface Product{
@@ -10,24 +11,35 @@ interface Product{
     name: string,
     price: number,
     images: string,
-    description: string
+    description: string,
+    userId: number
 };
+
+interface CartItem {
+    productId: number;
+    userId: number
+  }
 
 const OneProduct: React.FC = () => {
 
-    const [OneProduct, setOneProduct] = useState<Product>({id: 0, name: '', images: '', price: 0, description: ''});
-    
+    const [oneItem, setOneItem] = useState<Product>({id: 0, name: '', images: '', price: 0, description: '', userId:0});
+    const {name} = useParams()
 
   useEffect(()=>{
-        const name=window.location.href
-        axios.get<Product>(`http://localhost:3000/client/searchProductByName/${name}`)
+      
+        axios.get(`http://localhost:3000/client/searchProductByName/${name}`)
         .then((res)=>{    
-            setOneProduct(res.data)
+            setOneItem(res.data)
         })
         .catch((err) => {
                 console.log(err);
               });
-            },[])
+            },[name])
+
+      const add=(obj:CartItem)=>{
+        axios.post(`http://localhost:3000/Cart/add`,obj)
+         .then(r=>console.log('added',r)).catch(err=>console.log(err))
+        }
 
      return (
     <div>
@@ -37,11 +49,14 @@ const OneProduct: React.FC = () => {
         <div className="flex flex-col md:flex-row -mx-4">
             <div className="md:flex-1 px-4">
                 <div className="h-[460px] rounded-lg bg-gray-300 dark:bg-gray-700 mb-4">
-                    <img className="w-full h-full object-cover" src={OneProduct.images} alt="Product Image"/>
+                    <img className="w-full h-full object-cover" src={oneItem.images} alt="Product Image"/>
                 </div>
                 <div className="flex -mx-2 mb-4">
                     <div className="w-1/2 px-2">
-                        <button className="w-full bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600">Add to Cart</button>
+                        <button className="w-full bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600"
+                        onClick={()=>
+                            add({userId:oneItem.userId ,productId:oneItem.id})
+                              }>Add to Cart</button>
                     </div>
                     <div className="w-1/2 px-2">
                         <button className="w-full bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600">Add to Wishlist</button>
@@ -49,14 +64,11 @@ const OneProduct: React.FC = () => {
                 </div>
             </div>
             <div className="md:flex-1 px-4">
-                <h2 className="text-2xl font-bold text-gray-800 dark:text-black mb-2">Product Name</h2>
-                <p className="text-gray-600 dark:text-gray-300 text-sm mb-4">
-                    {OneProduct.name}
-                </p>
+                <h2 className="text-2xl font-bold text-gray-800 dark:text-black mb-2">{oneItem.name}</h2>
                 <div className="flex mb-4">
                     <div className="mr-4">
                         <span className="font-bold text-gray-700 dark:text-gray-300">Price:</span>
-                        <span className="text-gray-600 dark:text-gray-300">{OneProduct.price}</span>
+                        <span className="text-gray-600 dark:text-gray-300">{oneItem.price}</span>
                     </div>
                     <div>
                         <span className="font-bold text-gray-700 dark:text-gray-300">Availability:</span>
@@ -65,7 +77,7 @@ const OneProduct: React.FC = () => {
                 </div>
                 <div>
                     <span className="font-bold text-gray-700 dark:text-gray-300">Product Description:</span>
-                    <p className="text-gray-600 dark:text-gray-300 text-sm mt-2">{OneProduct.description}</p>
+                    <p className="text-gray-600 dark:text-gray-300 text-sm mt-2">{oneItem.description}</p>
                 </div>
             </div>
         </div>
